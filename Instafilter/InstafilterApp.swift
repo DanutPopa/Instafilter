1a7c28b9531cda218601cde1d931d87a3a09366f
DanutPopa <popa.ion.danut@gmail.com>
2024-10-15 20:00:43 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Danut Popa on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
