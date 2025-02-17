ed86e83c33377501a5f0dc0e1d88d7e5f60dfb21
DanutPopa <popa.ion.danut@gmail.com>
2025-02-17 16:43:10 +0200

    [Feature] Basic image filtering using Core Image.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Danut Popa on 15.10.2024.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import PhotosUI
 import SwiftUI
 
@@ -13,6 +15,9 @@ struct ContentView: View {
     @State private var filterIntensity = 0.5
     
     @State private var selectedItem: PhotosPickerItem?
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
         NavigationStack {
@@ -36,6 +41,7 @@ struct ContentView: View {
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
                 }
                 .padding(.vertical)
                 
@@ -61,8 +67,20 @@ struct ContentView: View {
             guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
             guard let inputImage = UIImage(data: imageData) else { return }
             
-            // more code to come
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
         }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return  }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
 
